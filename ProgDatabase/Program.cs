c7a0e93a1cde5f4b112e66154595863fe3713f35
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-11-22 15:16:39 +0100

    fixes

--- ProgDatabase/Program.cs
+++ ProgDatabase/Program.cs
@@ -12,9 +12,10 @@ namespace ProgDatabase
 
             database.AddReview(new Review("Positive", "Very good game, I like the story"));
             database.AddReview(new Review("Negative", "The game is to hard and you can't change the controls"));
+            database.AddReview(new Review("Mixed", "The game is okay. Not bad not good"));
 
             foreach (Review review in database.GetAllReviews()) {
-                Console.WriteLine(review.value + " " + review.message);
+                Console.WriteLine("Review is:" + " " + review.value + "; " + review.message);
             }
         }
     }
